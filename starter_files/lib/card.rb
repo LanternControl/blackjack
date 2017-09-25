6e1d344954e1a9ad6db7ef1644ff8d8b1af343a9
LanternControl <brett4president@hotmail.com>
2017-09-24 20:06:08 -0400

    Finished card class

--- starter_files/lib/card.rb
+++ starter_files/lib/card.rb
@@ -1,2 +1,11 @@
 class Card
-  
+  RANKS = [:A, 2, 3, 4, 5, 6, 7, 8, 9, 10, :J, :Q, :K]
+  SUITS = [:clubs, :diamonds, :hearts, :spades]
+
+  attr_reader :rank, :suit
+
+  def initialize(rank, suit)
+    @rank = rank
+    @suit = suit
+  end
+end
